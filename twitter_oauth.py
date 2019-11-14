7c82251a5e9ce4d6e7d6fddd6497090b08ce2849
零細SE <42573936+beginerSE@users.noreply.github.com>
2019-11-14 14:50:49 +0900

    Update twitter_oauth.py

--- twitter_oauth.py
+++ twitter_oauth.py
@@ -73,37 +73,3 @@ def get_twitter_access_token(oauth_token, oauth_verifier):
     access_token = dict(parse_qsl(response.content.decode("utf-8")))
 
     return access_token
-
-
-def get_accountdata(_id, oauth_token, oauth_secret):
-    """[アクセストークンからアカウントデータを取得する関数]
-
-    Arguments:
-        _id {[str]} -- [description]
-        oauth_token {[str]} -- [description]
-        oauth_secret {[str]} -- [description]
-
-    Returns:
-        [json]- [json形式のアカウントデータ]]
-    """
-
-    params = {
-        'user_id': _id,
-        'exclude_replies': True,
-        'include_rts': json.get('include_rts', False),
-        'count': 20,
-        'trim_user': False,
-        'tweet_mode': 'extended',    # full_textを取得するために必要
-    }
-
-    twitter = OAuth1Session(
-        consumer_key,
-        consumer_secret,
-        oauth_token,
-        oauth_secret
-    )
-
-    response = twitter.get(user_timeline_url, params=params)
-    results = json.loads(response.text)
-
-    return results
